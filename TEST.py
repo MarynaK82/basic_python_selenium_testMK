0267e9ad27cf43c77efc30d87bc4daa5d2bceed6
Maryna Kaminska <kaminska.maryna@gmail.com>
2019-10-14 21:13:30 +0300

    Assert is added: Test 1 automation

--- TEST.py
+++ TEST.py
@@ -7,4 +7,5 @@ search_button = driver.find_element_by_xpath("//form[@id = 'search-form']/fields
 search_field.send_keys("Test Automation")
 search_button.click("//form[@id = 'search-form']/fieldset/button")
 assert "Test automation" in driver.title
+assert "Test 1 automation" in driver.title
 driver.quit()
