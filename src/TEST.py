584630d82f87d5573015580355c4e8292786d479
Maryna Kaminska <kaminska.maryna@gmail.com>
2019-10-16 20:12:22 +0300

    test 1

--- src/TEST.py
+++ src/TEST.py
@@ -4,7 +4,7 @@ driver.get("https://www.wikipedia.org/")
 search_field = driver.find_element_by_id("searchInput").send_keys("Test Automation")
 search_button = driver.find_element_by_xpath("//form[@id = 'search-form']/fieldset/button")
 
-# search_field.send_keys("")
+#search_field.send_keys("Test Automation")
 search_button.click()
 assert "Test automation" in driver.title
 assert "Test 1 automation" in driver.title
